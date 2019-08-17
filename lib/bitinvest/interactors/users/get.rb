528f05a9612480070b09d1f8756e0d9951478ab2
rodrigopk <rodrigopk@gmail.com>
2019-08-17 17:05:09 +0200

    F [#25298723]: Add interactor to fetch user for id

---
+++ lib/bitinvest/interactors/users/get.rb
@@ -0,0 +1,26 @@
+require_relative '../interactor'
+
+module Interactors
+  module Users
+    class Get
+      include Bitinvest::Interactor
+
+      expose :user
+
+      USER_NOT_FOUND_ERROR = :user_not_found
+
+      def initialize(user_id, dependencies = {})
+        @user_repository = dependencies.fetch(:user_repository) do
+          UserRepository.new
+        end
+
+        @user_id = user_id
+      end
+
+      def call
+        @user = @user_repository.find(@user_id)
+        error!(USER_NOT_FOUND_ERROR) if @user.nil?
+      end
+    end
+  end
+end
