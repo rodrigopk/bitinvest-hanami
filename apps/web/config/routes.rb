97c8f1115ce6f6d433c2834329db075088c59c2f
rodrigopk <rodrigopk@gmail.com>
2019-08-18 19:58:07 +0200

    F [#25298520]: Add root route for web app to users#new

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -6,6 +6,7 @@
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 
-get '/users/new', to: 'users#new', as: 'users_new'
+root to: 'users#new'
+get '/users/signup', to: 'users#new', as: 'users_new'
 post '/users/signup', to: 'users#signup', as: 'users_signup'
 get '/users/:id', to: 'users#show', as: 'users_show'
